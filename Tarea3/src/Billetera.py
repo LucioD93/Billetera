db4c338c347cda00e4f10facfebdc05198846aca
LucioD93 <luciodavid93@gmail.com>
2017-02-01 12:56:36 -0400

    Creada clase billetera

---
+++ Tarea3/src/Billetera.py
@@ -0,0 +1,71 @@
+'''
+Created on 31 ene. 2017
+
+@author: luciod
+'''
+
+class Movimiento(object):
+    '''
+    Clase Movimiento, guarda informacion de cada movimiento
+    '''
+    def __init__(self, monto, fecha, idEstablecimiento):
+        '''
+        Constructor de la clase Movimiento
+        '''
+        self.monto = monto
+        self.fecha = fecha
+        self.idEstablecimiento = idEstablecimiento
+    
+    def monto(self):
+        '''
+        Get del atributo monto
+        '''
+        return self.monto
+
+class Billetera(object):
+    '''
+    Clase Billetera, guarda toda la informacion de la billetera
+    '''
+
+    def __init__(self, ID, nombre, apellido, ci, pin):
+        '''
+        Constructor de la clase Billetera
+        '''
+        self.ID = ID
+        self.nombre = nombre
+        self.apellido = apellido
+        self.pin = pin;
+        self.creditos = []
+        self.debitos = []
+    
+    def saldo(self):
+        '''
+        Metodo para obtener el saldo en una billetera
+        '''
+        saldo = 0
+        for movimiento in self.creditos:
+            saldo += movimiento.monto()
+        for movimiento in self.debitos:
+            saldo -= movimiento.monto()
+        
+    def recargar(self, monto, fecha, idEstablecimiento):
+        '''
+        Metodo para recargar saldo en una billetera
+        '''
+        recarga = Movimiento(monto, fecha, idEstablecimiento)
+        self.creditos.append(recarga)
+    
+    def consumir(self,monto, fecha, idEstablecimiento, pin):
+        '''
+        Metodo para consumir saldo en una billetera
+        '''
+        assert(self.pin == pin), "PIN incorrecto"
+        assert(self.saldo() > monto), "Saldo insuficiente"
+        consumo = Movimiento(monto, fecha, idEstablecimiento)
+        self.debitos.append(consumo)
+    
+    
+    
+    
+    
+        
--- Tarea3/src/__init__.py
+++ Tarea3/src/__init__.py
@@ -3,7 +3,12 @@ Created on 28 ene. 2017
 
 @author: luciod
 '''
+import unittest
+
+class PruebaBilletera(unittest.TestCase):
+    
+    def test(self):
+        pass
 
 if __name__ == '__main__':
-    print("Hola Mundo")
-    print("Prueba")
+    unittest.main()

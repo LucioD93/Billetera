37d4e2f09b26dde61f584994ee4ee9c34d2cb0c4
LucioD93 <luciodavid93@gmail.com>
2017-02-02 14:51:27 -0400

    Mas pruebas unitarias agregadas

--- Tarea3/src/__init__.py
+++ Tarea3/src/__init__.py
@@ -32,7 +32,32 @@ class PruebaBilletera(unittest.TestCase):
         with self.assertRaises(Exception):
             b.consumir(125, datetime.strptime("07/27/2012", "%m/%d/%Y"), 1, 1234)
     
+    def testRecargasMultiples(self):
+        b = Billetera(1, "Pedro", "Perez", 7573920, 1234)
+        for i in range (50):
+            b.recargar(100, datetime.strptime("07/27/2012", "%m/%d/%Y"), 1)
+        self.assertEqual(b.saldo(),5000)
+
+    def testConsumosMultiples(self):
+        b = Billetera(1, "Pedro", "Perez", 7573920, 1234)
+        for i in range (50):
+            b.recargar(100, datetime.strptime("07/27/2012", "%m/%d/%Y"), 1)
+        self.assertEqual(b.saldo(),5000)
+        for i in range (50):
+            b.consumir(25, datetime.strptime("07/27/2012", "%m/%d/%Y"), 1, 1234)        
+        self.assertEqual(b.saldo(),3750)
         
+    def testConsumosYRecargas(self):
+        b = Billetera(1, "Pedro", "Perez", 7573920, 1234)
+        for i in range (50):
+            b.recargar(100, datetime.strptime("07/27/2012", "%m/%d/%Y"), 1)
+            b.consumir(25, datetime.strptime("07/27/2012", "%m/%d/%Y"), 1, 1234)
+        self.assertEqual(b.saldo(),3750)
+    
+    def testRecargaNegativa(self):
+        b = Billetera(1, "Pedro", "Perez", 7573920, 1234)
+        with self.assertRaises(Exception):
+            b.recargar(-23, datetime.strptime("07/27/2012", "%m/%d/%Y"), 1)        
                
 if __name__ == '__main__':
     unittest.main()
